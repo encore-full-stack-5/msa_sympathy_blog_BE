91cf3b27714544dcd8d37530e57745a38206fb96
heeseo333 <heeseo310@gmail.com>
2024-05-03 17:39:10 +0900

    UserBlogEntity

--- user/src/main/java/com/example/user/global/domain/entity/UserBlog.java
+++ user/src/main/java/com/example/user/global/domain/entity/UserBlog.java
@@ -1,4 +1,20 @@
 package com.example.user.global.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "USER_BLOGS")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter @Builder
 public class UserBlog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
+    private Long id;
+    @Column(name = "NICKNAME")
+    private String nickname;
+    @Column(name = "BLOG_NAME")
+    private String blogName;
 }
