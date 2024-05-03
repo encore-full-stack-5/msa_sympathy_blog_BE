72968f9cda3c0ad7692295eb2f786af0a89dd760
lsy <sylim0513@gmail.com>
2024-05-03 17:19:31 +0900

    [Feat postcategory,postview]

--- post/src/main/java/com/example/post/global/domain/repository/PostRepository.java
+++ post/src/main/java/com/example/post/global/domain/repository/PostRepository.java
@@ -1,6 +1,8 @@
 package com.example.post.global.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository {
+
 }
