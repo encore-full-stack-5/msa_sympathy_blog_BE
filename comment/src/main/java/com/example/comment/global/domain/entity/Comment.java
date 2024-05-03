d5b26160b6b3b8066cd9500b3a1fc32c7fdee511
heeseo333 <heeseo310@gmail.com>
2024-05-03 16:48:13 +0900

    Entity&REPO

--- comment/src/main/java/com/example/comment/global/domain/entity/Comment.java
+++ comment/src/main/java/com/example/comment/global/domain/entity/Comment.java
@@ -1,4 +1,31 @@
 package com.example.comment.global.domain.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name ="COMMENTS")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="COMMENT_ID")
+    private Long id;
+    @Column(name="COMMENT_CREATED_AT", nullable = false)
+    private LocalDateTime createdAt;
+    @Column(name = "POST_ID")
+    private Long postId;
+    @Column(name = "USER_ID")
+    private Long userId;
+
+
 }
