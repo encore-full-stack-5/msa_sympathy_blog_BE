3285fec5029e8697aae6127f44909a545a07e52f
heeseo333 <heeseo310@gmail.com>
2024-05-07 18:16:23 +0900

    f

--- comment/src/main/java/com/example/comment/global/domain/entity/CommentLike.java
+++ comment/src/main/java/com/example/comment/global/domain/entity/CommentLike.java
@@ -12,13 +12,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name ="LIKES")
-public class Like {
+public class CommentLike {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "LIKE_ID")
     private Long id;
     @Column(name = "IS_LIKED")
-    Boolean like;
+    Boolean isLiked;
     @Column(name = "POST_ID")
     Long postId;
     @Column(name = "USER_ID")
