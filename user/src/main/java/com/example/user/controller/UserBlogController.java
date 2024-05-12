a2c580b46cdf6bd095d489e2cc77bfffd88c89e7
m48483 <71882579+m48483@users.noreply.github.com>
2024-05-12 17:20:18 +0900

    commit too dificult

--- user/src/main/java/com/example/user/controller/UserBlogController.java
+++ user/src/main/java/com/example/user/controller/UserBlogController.java
@@ -4,6 +4,7 @@ import com.example.user.dto.request.SignInRequest;
 import com.example.user.dto.request.SignUpRequest;
 import com.example.user.dto.request.TeamRequest;
 import com.example.user.dto.response.SignInResponse;
+import com.example.user.dto.response.TokenResponse;
 import com.example.user.dto.response.UserBlogResponse;
 import com.example.user.global.domain.entity.UserBlog;
 import com.example.user.service.TokenService;
@@ -13,6 +14,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,15 +28,13 @@ public class UserBlogController {
     public SignInResponse signIn(@RequestBody SignInRequest request) {
         return userBlogService.signIn(request);
     }
-    @PostMapping("token")
-    @RolesAllowed({"USER"})
-    public UserBlogResponse getUserResponse(@RequestBody TeamRequest request
-            , @AuthenticationPrincipal UserBlog user) {
-        tokenService.isAuthenticatedTeam(request);
-        return new UserBlogResponse(
-                user.getId().toString(),
-                user.getEmail(),
-                user.getNickname()
-        );
+    @GetMapping
+    public TokenResponse kickToken(@RequestParam(name = "token") String token) {
+        return userBlogService.passToken(token);
+    }
+
+    @GetMapping("/parse")
+    public String parseToken(@RequestParam(name = "token") String token) {
+        return userBlogService.parseToken(token);
     }
 }
